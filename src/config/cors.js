36b802610344ec420f81e17daec72da94e47ff10
Kamenrraiderx <alejandro.reyes95@yahoo.com>
2024-11-18 16:13:24 -0600

    add domain of application

--- src/config/cors.js
+++ src/config/cors.js
@@ -2,7 +2,7 @@ const  cors =  require('cors') ;
 
 // Configuración de opciones de CORS
 const corsOptions = {
-  origin: 'http://localhost:3001',
+  origin: ['http://localhost:3001','https://ena-trasportes.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization','Access-Control-Allow-Headers', 'Origin', 'X-Requested-With' , 'Accept'],
 };
